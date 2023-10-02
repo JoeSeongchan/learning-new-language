fa7d83fcd0fe7f3d8eba1b08832aa0e096f9995b
seongchan cho <wordnumperiod@gmail.com>
2023-10-02 19:00:56 +0900

    update: 루비 20분 가이드 코드 업로드
    루비 공식 문서 '루비 20분 가이드 (Quick start)'를 보고 따라 친 코드를 업로드합니다.

---
+++ ruby/src/hello_world.rb
@@ -0,0 +1,49 @@
+# ruby 20분 가이드를 보고 따라 친 코드입니다.
+# https://www.ruby-lang.org/ko/documentation/quickstart/4/
+class MegaGreeter
+  attr_accessor :names
+
+  def initialize(names = "World")
+    @names = names
+  end
+
+  def say_hi
+    if @names.nil?
+      puts "..."
+    elsif @names.respond_to?("each")
+      @names.each do |name|
+        puts "Hello #{name}!"
+      end
+    else
+      puts "Hello #{@names}!"
+    end
+  end
+
+  def say_bye
+    if @names.nil?
+      puts "..."
+    elsif @names.respond_to?("join")
+      puts "Goodbye #{@names.join(", ")}. Come back soon!"
+    else
+      puts "Goodbye #{@names}. Come back soon!"
+    end
+  end
+end
+
+if __FILE__ == $0
+  mg = MegaGreeter.new
+  mg.say_hi
+  mg.say_hi
+
+  mg.names = "Zeke"
+  mg.say_hi
+  mg.say_bye
+
+  mg.names = ["Albert","Brenda","Charles","Dave","Engelbert"]
+  mg.say_hi
+  mg.say_bye
+
+  mg.names = nil
+  mg.say_hi
+  mg.say_bye
+end
